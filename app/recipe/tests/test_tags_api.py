b6672d28978c606e72125591577d96e66a0c0288
Vladimir Matyagin <ghost-email@yandex.ru>
2021-01-05 22:13:50 +0300

    + | create add feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -12,7 +12,7 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
-class PublickTagsApiTests(TestCase):
+class PublicTagsApiTests(TestCase):
     """test the publicly available tags api"""
 
     def setUp(self):
@@ -64,3 +64,22 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """test creating a new tag"""
+        payload = {
+            'name': 'test tag'
+        }
+        self.client.post(TAGS_URL, payload)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
