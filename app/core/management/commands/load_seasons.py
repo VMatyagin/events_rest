a4d4bb443da44d58e690b84c908be36fd44ad154
Vladimir Matyagin <ghost-email@yandex.ru>
2021-02-27 21:36:46 +0300

    * | added parsing fns

---
+++ app/core/management/commands/load_seasons.py
@@ -0,0 +1,44 @@
+import json
+
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_str
+
+from core.models import Boec, Brigade, Season
+
+
+class Command(BaseCommand):
+    """Parse JSON file and load data to DB"""
+
+    def handle(self, *args, **options):
+        with open('data.json', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+        for item in data:
+            try:
+                fio = item['name'].split()
+
+                middleName = fio[2] if len(fio) > 2 else ''
+                lastName = fio[0] if len(fio) > 1 else ''
+                firstName = fio[1] if len(fio) > 1 else ''
+
+                boecList = Boec.objects.filter(
+                    firstName=firstName, lastName=lastName, middleName=middleName
+                )
+
+                if (not boecList.exists()):
+                    Boec.objects.create(
+                        firstName=firstName, lastName=lastName, middleName=middleName
+                    )
+
+                brigade = Brigade.objects.get(title=item['brigade'])
+                boec = Boec.objects.get(
+                    firstName=firstName, lastName=lastName, middleName=middleName
+                )
+
+                if (not Season.objects.filter(
+                    boec=boec, brigade=brigade, year=int(item['year'])
+                ).exists()):
+                    Season.objects.create(
+                        boec=boec, brigade=brigade, year=int(item['year']))
+            except Exception:
+                print(item)
---
+++ app/core/management/commands/load_so.py
@@ -0,0 +1,49 @@
+import json
+
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_str
+
+from core.models import Area, Brigade, Shtab
+
+
+class Command(BaseCommand):
+    """Parse JSON file and load data to DB"""
+
+    area = None
+    shtab = None
+
+    def findArea(self, area):
+        areaList = Area.objects.filter(
+            shortTitle=area
+        )
+        if (not areaList.exists()):
+            Area.objects.create(
+                title=force_str(''), shortTitle=_(area))
+
+        self.area = Area.objects.get(shortTitle=_(area))
+
+    def findShtab(self, shtab):
+        shtabList = Shtab.objects.filter(title=shtab)
+
+        if (not shtabList.exists()):
+            Shtab.objects.create(title=shtab)
+
+        self.shtab = Shtab.objects.get(
+            title=shtab)
+
+    def handle(self, *args, **options):
+        with open('brigades.json', encoding='utf-8',
+                  errors='surrogateescape') as json_file:
+            data = json.load(json_file)
+        for item in data:
+            self.findArea(item['direction'])
+            self.findShtab(item['shtab'])
+
+            brigade = Brigade.objects.filter(
+                title=item['name']
+            )
+
+            if (not brigade.exists()):
+                Brigade.objects.create(
+                    title=item['name'], area=self.area, shtab=self.shtab)
