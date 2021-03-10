3de7fcd101a6423841058ae3aadaf922f0f8d871
Vladimir Matyagin <ghost-email@yandex.ru>
2021-03-10 17:44:36 +0300

    * | Event Users | added deleting & updating

--- app/event/views.py
+++ app/event/views.py
@@ -1,11 +1,12 @@
+from django.core.exceptions import ValidationError
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from core.authentication import VKAuthentication
-from rest_framework import filters
+from rest_framework import filters, status
 
 from reversion.views import RevisionMixin
 
-from core.models import Event, EventOrder
+from core.models import Boec, Brigade, Event, EventOrder
 from event import serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -39,25 +40,39 @@ class EventViewSet(RevisionMixin, viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
-    @action(methods=['get'], detail=True, permission_classes=(IsAuthenticated, ),
+    def handleUsers(self, request, pk, type='volonteer'):
+        event = Event.objects.get(pk=pk)
+        list = getattr(event, type)
+        if request.method == 'POST':
+            """add user to type to event"""
+            try:
+                id = request.data['id']
+                boec = Boec.objects.get(id=id)
+                list.add(boec)
+            except (Boec.DoesNotExist, ValidationError):
+                raise status.HTTP_400_BAD_REQUEST
+        if request.method == 'DELETE':
+            """delete user from type of event"""
+            try:
+                id = request.data['id']
+                boec = Boec.objects.get(id=id)
+                list.remove(boec)
+            except (Boec.DoesNotExist, ValidationError):
+                raise status.HTTP_400_BAD_REQUEST
+
+        """get users list"""
+        serializer = BoecShortSerializer(list, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['get', 'post', 'delete'], detail=True, permission_classes=(IsAuthenticated, ),
             url_path='volonteers', url_name='volonteers')
-    def get_volonteers(self, request, pk=None):
-        """get volonteers list"""
-        event = Event.objects.get(pk=pk)
+    def handle_volonteers(self, request, pk=None):
+        return self.handleUsers(request=request, pk=pk, type='volonteer')
 
-        serializer = BoecShortSerializer(event.volonteer, many=True)
-
-        return Response(serializer.data)
-
-    @action(methods=['get'], detail=True, permission_classes=(IsAuthenticated, ),
+    @action(methods=['get', 'post', 'delete'], detail=True, permission_classes=(IsAuthenticated, ),
             url_path='organizers', url_name='organizers')
-    def get_organizers(self, request, pk=None):
-        """get organizers list"""
-        event = Event.objects.get(pk=pk)
-
-        serializer = BoecShortSerializer(event.organizer, many=True)
-
-        return Response(serializer.data)
+    def handle_organizers(self, request, pk=None):
+        return self.handleUsers(request=request, pk=pk, type='organizer')
 
     @action(methods=['get'], detail=True, permission_classes=(IsAuthenticated, ),
             url_path='orders', url_name='orders')
@@ -69,6 +84,7 @@ class EventViewSet(RevisionMixin, viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
+
 class EventOrdersViewSet(RevisionMixin, viewsets.ModelViewSet):
     """manage orders in the database"""
     serializer_class = serializers.OrderSerializer
@@ -76,3 +92,44 @@ class EventOrdersViewSet(RevisionMixin, viewsets.ModelViewSet):
     authentication_classes = (VKAuthentication,)
     permission_classes = (IsAuthenticated, )
     paginator = None
+
+    def validate_ids(self, id_list, model):
+        for id in id_list:
+            try:
+                model.objects.get(id=id)
+            except (model.DoesNotExist, ValidationError):
+                raise status.HTTP_400_BAD_REQUEST
+        return True
+
+    def update(self, request, *args, **kwargs):
+        if 'brigades_id' in request.data:
+            id_list = request.data['brigades_id']
+
+            self.validate_ids(id_list=id_list, model=Brigade)
+            instances = []
+            for id in id_list:
+                obj = Brigade.objects.get(id=id)
+                instances.append(obj)
+            order = EventOrder.objects.get(id=request.data['id'])
+            order.brigades.set(instances)
+            order.save()
+
+            del request.data['brigades_id']
+
+        if 'participations' in request.data:
+            id_list = list(map(
+                lambda x: x['id'], request.data['participations']
+            ))
+
+            self.validate_ids(id_list=id_list, model=Boec)
+            instances = []
+            for id in id_list:
+                obj = Boec.objects.get(id=id)
+                instances.append(obj)
+            order = EventOrder.objects.get(id=request.data['id'])
+            order.participations.set(instances)
+            order.save()
+
+            del request.data['participations']
+
+        return super().update(request, *args, **kwargs)
