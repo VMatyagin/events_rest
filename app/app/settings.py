fab3ab5257d25d224b5be6f881fe91f9931f7a98
Vladimir Matyagin <ghost-email@yandex.ru>
2021-03-22 16:13:06 +0300

    + | Settings | added vercel support

--- app/app/settings.py
+++ app/app/settings.py
@@ -142,6 +142,7 @@ AUTH_USER_MODEL = 'core.User'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://vk-mini-so.vercel.app",
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.ngrok\.io$",
