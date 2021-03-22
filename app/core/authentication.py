d5ce6afce6b4eb12f148fb09143ae5f94853a7eb
Vladimir Matyagin <ghost-email@yandex.ru>
2021-03-22 14:28:39 +0300

    * | Authentication | changed description

--- app/core/authentication.py
+++ app/core/authentication.py
@@ -58,15 +58,14 @@ def is_valid(query: dict, secret: str) -> bool:
 
 class VKAuthentication(BaseAuthentication):
     """
-    Simple token based authentication.
+    Authentication based on vk sign keys.
 
-    Clients should authenticate by passing the token key in the "Authorization"
-    HTTP header, prepended with the string "Token ".  For example:
-
-        Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
+    Clients should authenticate by passing the current location "Authorization"
+    HTTP header.  For example:
+        Authorization: vk_user_id=494075&vk_app_id=6736218&vk_is_app_user=1&vk_are_notifications_enabled=1&vk_language=ru&vk_access_token_settings=&vk_platform=android&sign=htQFduJpLxz7ribXRZpDFUH-XEUhC9rBPTJkjUFEkRA
     """
 
-    keyword = 'VK'
+    keyword = ''
 
     def authenticate(self, request):
         auth = get_authorization_header(request).split()
