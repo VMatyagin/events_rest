690e2408e83cf5c55bfe950e7212d33ac3f91aba
Vladimir Matyagin <ghost-email@yandex.ru>
2021-06-11 18:07:38 +0300

    + | trying to get access to goggle sheets

---
+++ app/core/management/commands/table.py
@@ -0,0 +1,18 @@
+import json
+import pygsheets
+import numpy as np
+import datetime
+
+from django.core.management.base import BaseCommand
+
+from core.models import Boec, Brigade, Season
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        client = pygsheets.authorize()
+
+        sht = client.open_by_key('1s_NVTmYxG5GloDaOOw4d7eh7P_zAcobTmIRseYHsg3g')
+        sht.add_worksheet(f"{datetime.date.today()}")
